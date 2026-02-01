0b8d8a39ceb7633186e6adce53b082f1344ae338
kimtsuny <altyym59@gmail.com>
2026-02-01 05:13:08 +0300

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <p className='text-green-500 font-extrabold'>
+      uihyuhbuihuih879huj980j
+     </p>
+    </>
+  )
+}
+
+export default App
