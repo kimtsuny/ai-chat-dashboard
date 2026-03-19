5a222da9f31e58f04d374f7e601b6f897bb0431a
kimtsuny <altyym59@gmail.com>
2026-03-19 12:09:54 +0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
@@ -6,7 +5,7 @@ import App from "./App.tsx";
 import { AuthProvider } from "@/context/AuthContext.tsx";
 import { ChatProvider } from "@/context/ChatContext.tsx";
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>  
+ 
     <BrowserRouter>
     <AuthProvider>
       <ChatProvider>
@@ -14,5 +13,5 @@ createRoot(document.getElementById("root")!).render(
       </ChatProvider>
       </AuthProvider>
     </BrowserRouter>
-    </StrictMode>
+    
 );
